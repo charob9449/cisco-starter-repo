96d54e3dc1562de4a0aa5e865cdc0186a9786ab0
charob9449 <117673156+charob9449@users.noreply.github.com>
2023-02-02 04:14:28 -0500

    Update App.js

--- Sextant/SRC/App.js
+++ Sextant/SRC/App.js
@@ -1,43 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
+import { Banner } from './components/banner';
+import {Exhibit, Exhibit1, Latency} from './components/exhibit';
 function App() {
 
-  const [details, setDetails] = useState(null);
-
-  const getUserGeolocationDetails= () =>{
-    fetch("https://geolocation-db.com/json/a9e48c70-8b22-11ed-8d13-bd165d1291e3")
-    .then(response => response.json() )
-    .then( data => setDetails( data ) );
-  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Get my IP and Location.
-        </p>
-        <p
-          className="mt-3"
-         >
-          <button className="btn btn-primary" onClick={getUserGeolocationDetails}>
-            Find my details
-          </button>
-          <div className="row justify-content-center mt-3">
-            <div className="col-lg-6 text-center text-dark">
-              {details && <ul className="list-group">
-                <li className="list-group-item">
-                  Location : { `${details.city}, ${details.country_name}(${details.country_code})` }
-                </li>
-                <li className="list-group-item">IP {details.IPv4}</li>
-              </ul>}
-            </div>
+    
+      <div className="App">
+        <header className="App-header">
+          <Banner/>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Exhibit/>
+          <Exhibit1/>
+          
+          <Latency/>
+          
 
-          </div>
-         </p>
-      </header>
-    </div>
+        </header>
+        
+      </div>
+      
   );
 }
 
